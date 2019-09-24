5d0ef669c98ad395b39d9565f185dede7051fe38
Chao Hsuan Chi <allen0413@gmail.com>
2019-09-24 22:58:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,8 +10,8 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/display_puppy' do
-
+  post '/puppy' do
+    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
     erb :display_puppy
   end
 
